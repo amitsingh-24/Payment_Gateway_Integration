9f24d0c6df93e455e0b8fd561a6a572b3ce514c3
Amit Singh Rajawat <72181101+amitsingh-24@users.noreply.github.com>
2021-09-05 14:41:48 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -5,14 +5,14 @@ $name = $_POST['fullname'];
 $to = $_POST['Email'];
 $PaymentThrough= $_POST['Payment Through'];
 $DonationMoney= $_POST['Donation Money']
-$from = "imamitsingh958@gmail.com";
+$from = "noreply@amitsingh-24.github.io";
 $subject = "Mail From Donation website";
 $txt ="Name = ". $fullname . "\r\n  Email = " . $Email . "\r\n PaymentThrough =" . $PaymentThrough . "\r\n DonationMoney =" . $DonationMoney .;
 $headers = "From: noreply@amitsingh-24.github.io" . "\r\n" .
 
 }
 if($email!=NULL){
-    mail($to,$subject,$txt,$headers);
+    mail($from,$subject,$txt,$headers);
 }
 //redirect
 header("Location:thankyou.html");
