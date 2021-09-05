628249440683a2ba41d5683990d59a1cecff8377
Amit Singh Rajawat <72181101+amitsingh-24@users.noreply.github.com>
2021-09-05 14:14:13 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,15 +1,16 @@
 <?php
 //get data from form
-
+if(isset($_POST['Submit'])){
 $name = $_POST['fullname'];
-$email= $_POST['Email'];
+$to = $_POST['Email'];
 $PaymentThrough= $_POST['Payment Through'];
 $DonationMoney= $_POST['Donation Money']
-$to = "imamitsingh958@gmail.com";
-$subject = "Mail From website";
+$from = "imamitsingh958@gmail.com";
+$subject = "Mail From Donation website";
 $txt ="Name = ". $fullname . "\r\n  Email = " . $Email . "\r\n PaymentThrough =" . $PaymentThrough . "\r\n DonationMoney =" . $DonationMoney .;
 $headers = "From: noreply@amitsingh-24.github.io" . "\r\n" .
-"CC: somebodyelse@example.com";
+
+}
 if($email!=NULL){
     mail($to,$subject,$txt,$headers);
 }
