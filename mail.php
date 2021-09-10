3f80d989bef269866bafc1a7ffcf822d7249b546
Amit Singh Rajawat <72181101+amitsingh-24@users.noreply.github.com>
2021-09-10 18:16:49 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -1,16 +1,15 @@
  
 <?php
 //get data from form
-if(isset($_POST['Submit'])){
-    $fullname = $_POST['fullname'];
-    $to = $_POST['Email'];
-    $PaymentThrough= $_POST['Payment Through'];
-    $DonationMoney= $_POST['Donation Money'];
-    $from = "noreply@amitsingh-24.github.io";
-    $subject = "Mail From Donation website";
-    $txt ="Name = ". $fullname . "\r\n  Email = " . $Email . "\r\n PaymentThrough =" . $PaymentThrough . "\r\n DonationMoney =" . $DonationMoney .;
-    $headers = "From: noreply@amitsingh-24.github.io" . "\r\n" .;
-}
+$fullname = $_POST['fullname'];
+$to = $_POST['Email'];
+$PaymentThrough= $_POST['Payment Through'];
+$DonationMoney= $_POST['Donation Money'];
+$from = "noreply@amitsingh-24.github.io";
+$subject = "Mail From Donation website";
+$txt ="Name = ". $fullname . "\r\n  Email = " . $Email . "\r\n PaymentThrough =" . $PaymentThrough . "\r\n DonationMoney =" . $DonationMoney .;
+$headers = "From: noreply@amitsingh-24.github.io" . "\r\n" .;
+
 if($email!=NULL){
     mail($from,$subject,$txt,$headers);
 }
