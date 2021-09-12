0350eb8f030413aeaf2961a462a70357e674bddd
Amit Singh Rajawat <72181101+amitsingh-24@users.noreply.github.com>
2021-09-12 21:17:54 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -2,22 +2,30 @@
 <?php
 ob_start();
 //get data from form
-if (isset($_POST['submit'])){ 
+if (isset($_POST['submit'])){
+ $subject = "Mail From Donation website";
  $message = "Thank you for Donating.";
  $fullname = $_POST['fullname'];
  $email = $_POST['Email'];
  $PaymentThrough= $_POST['PaymentThrough'];
  $DonationMoney= $_POST['DonationMoney'];
+ $headers = 'From:'. $email2 . "rn"; // Sender's Email
+ $headers .= 'Cc:'. $email2 . "rn"; // Carbon copy to Sender
  
-$to = "thisisamitsingh007@gmail.com";
-$subject = "Mail From Donation website";
-$txt ="Name: $fullname  \n  Email: $email  \n Payment Through:  $PaymentThrough  \n Donation Money:  $DonationMoney ";
+ 
+ mail("thisisamitsingh007@gmail.com", $subject, $message, $fullname, $email, $Paymentthrough, $DonationMoney, $headers);
+ 
+ 
+} 
+ 
+//$to = "thisisamitsingh007@gmail.com";
+//$subject = "Mail From Donation website";
+//$txt ="Name: $fullname  \n  Email: $email  \n Payment Through:  $PaymentThrough  \n Donation Money:  $DonationMoney ";
 //$headers = "From: noreply@amitsingh-24.github.io" . "\r\n" .;
 
+//mail($to,$subject,$txt);
 
-mail($to,$subject,$txt);
 
-}
 //redirect
 //header("Location:thankyou.html");
 ?>
