908a1731a7f7ee03b08a261d10c36798c9499b44
Amit Singh Rajawat <72181101+amitsingh-24@users.noreply.github.com>
2021-09-17 23:59:49 +0530

    Update mail.php

--- mail.php
+++ mail.php
@@ -3,13 +3,11 @@ ob_start(); //output buffering because I like it.
 
 if (isset($_POST['submit'])) {
   // Process the form
-  $message = "Thank you for Donating! We will respond to your request shortly";
+  $message = "Thank you for Donating! Thanks for Donating";
   $fullname = $_POST['fullname'];
   $email = $_POST['email'];
   $PaymentThrough = $_POST['PaymentThrough'];
   $DonationMoney = $_POST['DonationMoney'];
-  
-
 
 $to = "thisisamitsingh007@gmail.com";
 $subject = "Donation Reciept";
@@ -18,7 +16,6 @@ $body = " Donater Name/Name's: $fullname \n Donater E-mail: $email \n \n";
 mail($to,$subject,$body);
 
 }
-
 ?>
 
 
